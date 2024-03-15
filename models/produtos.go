07035c6decdb1858fc28ccfec1b8c29499a1d323
EleyOliveira <eleyoliveira@bol.com.br>
2024-03-15 15:22:56 -0300

    feat(cadastro): atualização de produto
    disponibilizada tela de edição de produto como a função que efetivamente atualiza o banco de dados

--- models/produtos.go
+++ models/produtos.go
@@ -1,6 +1,9 @@
 package models
 
-import "loja/db"
+import (
+	"log"
+	"loja/db"
+)
 
 type Produto struct {
 	Id         int
@@ -8,6 +11,39 @@ type Produto struct {
 	Descricao  string
 	Preco      float64
 	Quantidade int
+}
+
+func ConsultaProduto(id string) Produto {
+
+	db := db.ConectaBancoDados()
+	produto, err := db.Query("select * from produtos where id = $1", id)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	produtoRetorno := Produto{}
+
+	for produto.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = produto.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		produtoRetorno.Id = id
+		produtoRetorno.Descricao = descricao
+		produtoRetorno.Nome = nome
+		produtoRetorno.Preco = preco
+		produtoRetorno.Quantidade = quantidade
+	}
+
+	defer db.Close()
+
+	return produtoRetorno
+
 }
 
 func ConsultaProdutos() []Produto {
@@ -39,6 +75,45 @@ func ConsultaProdutos() []Produto {
 
 		produtos = append(produtos, p)
 	}
+	defer db.Close()
 
 	return produtos
+}
+
+func Incluir(nome, descricao string, preco float64, quantidade int) {
+	db := db.ConectaBancoDados()
+
+	inclusao, err := db.Prepare("insert into produtos (nome, descricao, preco, quantidade) values ($1, $2, $3, $4)")
+	if err != nil {
+		log.Println("Problemas na inclusao", err)
+	}
+
+	inclusao.Exec(nome, descricao, preco, quantidade)
+	defer db.Close()
+
+}
+
+func Deletar(id string) {
+	db := db.ConectaBancoDados()
+
+	delecao, err := db.Prepare("delete from produtos where id = $1")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	delecao.Exec(id)
+	defer db.Close()
+}
+
+func Editar(id, nome, descricao string, preco float64, quantidade int) {
+	db := db.ConectaBancoDados()
+
+	edicao, err := db.Prepare("update produtos set nome = $2, descricao = $3, preco = $4, quantidade = $5 where id = $1")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	edicao.Exec(id, nome, descricao, preco, quantidade)
+	defer db.Close()
 }
--- routes/routes.go
+++ routes/routes.go
@@ -7,4 +7,9 @@ import (
 
 func CarregarRotas() {
 	http.HandleFunc("/", controllers.Index)
+	http.HandleFunc("/inclusao", controllers.Inclusao)
+	http.HandleFunc("/insert", controllers.Incluir)
+	http.HandleFunc("/delete", controllers.Deletar)
+	http.HandleFunc("/edicao", controllers.Edicao)
+	http.HandleFunc("/edit", controllers.Editar)
 }
